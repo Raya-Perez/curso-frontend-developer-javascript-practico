6f27f2018720bbdb7827b9d21a0de01918c4bda0
Raya-Perez <rayaperez@gmail.com>
2022-08-16 17:28:48 -0500

    se_agrega_carrito_compras

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuMobilIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuMobilIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
@@ -12,4 +16,8 @@ function toggleDesktopMenu(){
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    aside.classList.toggle('inactive');
 }
