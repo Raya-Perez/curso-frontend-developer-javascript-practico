0e7c4aeae882f2a44c8c84f5792ba57a28d09d09
Raya-Perez <rayaperez@gmail.com>
2022-08-17 17:51:26 -0500

    update main

--- main.js
+++ main.js
@@ -10,14 +10,38 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 menuMobilIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+  
+    if (!isAsideClosed) {
+      aside.classList.add('inactive');
+     }
+    
     desktopMenu.classList.toggle('inactive');
-}
-
-function toggleMobileMenu(){
+  }
+  
+  function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+  
+    if (!isAsideClosed) {
+      aside.classList.add('inactive'); 
+    }
+    
     mobileMenu.classList.toggle('inactive');
-}
-
-function toggleCarritoAside(){
+  }
+  
+  function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')    
+    if (!isMobileMenuClosed) {
+      mobileMenu.classList.add('inactive');
+    }
+      if (!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+      }
+   
+        
     aside.classList.toggle('inactive');
-}
+  }
+
+ 
